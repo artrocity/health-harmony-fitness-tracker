0948cbe6b87e2506bd3b6a263a6ac7770e3a1f5e
Ryan Barfield <cadium828@gmail.com>
2024-02-28 19:20:00 -0600

    updated database.sql file with table entries

--- database.sql
+++ database.sql
@@ -1,9 +1,40 @@
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+-- Create Users Table
+CREATE TABLE "users" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Create Weight Table
+CREATE TABLE Weight (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "users"(id),
+    current_weight DECIMAL NOT NULL,
+    goal_weight DECIMAL NOT NULL
+);
+
+-- Create Food Table
+CREATE TABLE Food (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "users"(id),
+    food VARCHAR(255) NOT NULL,
+    calories INTEGER NOT NULL
+);
+
+-- Create Exercise Table
+CREATE TABLE Exercise (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "users"(id),
+    exercise VARCHAR(255) NOT NULL,
+    calories_burned INTEGER NOT NULL
+);
+
+-- Create Symptoms Table
+CREATE TABLE Symptoms (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "users"(id),
+    symptom VARCHAR(255) NOT NULL,
+    severity INTEGER NOT NULL,
+    overall_feeling INTEGER NOT NULL
 );
