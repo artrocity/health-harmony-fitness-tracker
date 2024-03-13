8e9c131ee88f67a55093ab49fde353be9881ecee
Ryan Barfield <cadium828@gmail.com>
2024-03-13 18:35:24 -0500

    updated initial styling of navbar

--- src/components/HorizontalNav/HorizontalNav.jsx
+++ src/components/HorizontalNav/HorizontalNav.jsx
@@ -1,31 +1,62 @@
-// Import 3rd Party Libraries
+// Installed 3rd Party
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link as RouterLink } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function HorizontalNav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">Logo</Link>
-        </li>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/features">Features</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/login">Log In</Link>
-        </li>
-      </ul>
-    </nav>
+    <AppBar position="static" sx={{ backgroundColor: '#782CF6' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={RouterLink} to="/home">
+            Logo
+          </Button>
+        </Typography>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/home"
+          sx={{ '&:hover': { backgroundColor: 'black' } }}
+        >
+          Home
+        </Button>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/home"
+          sx={{ '&:hover': { backgroundColor: 'black' } }}
+        >
+          Features
+        </Button>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/home"
+          sx={{ '&:hover': { backgroundColor: 'black' } }}
+        >
+          About
+        </Button>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/home"
+          sx={{ '&:hover': { backgroundColor: 'black' } }}
+        >
+          Contact
+        </Button>
+        <Button
+          color="inherit"
+          component={RouterLink}
+          to="/home"
+          sx={{ '&:hover': { backgroundColor: 'black' } }}
+        >
+          Log In
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
