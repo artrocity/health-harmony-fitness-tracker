4254ba68e056ffc2867dc0b2f1996306fa816102
Ryan Barfield <cadium828@gmail.com>
2024-03-13 19:47:48 -0500

    landing page component completed

--- src/components/HorizontalNav/HorizontalNav.jsx
+++ src/components/HorizontalNav/HorizontalNav.jsx
@@ -1,60 +1,42 @@
-// Installed 3rd Party
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { useLocation, Link as RouterLink } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 function HorizontalNav() {
+  const location = useLocation(); // Hook to access the current location
+
+  // Function to determine if the route is active
+  const isActive = (pathname) => location.pathname === pathname;
+
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#782CF6' }}>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}
+    >
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           <Button color="inherit" component={RouterLink} to="/home">
             Logo
           </Button>
         </Typography>
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/home"
-          sx={{ '&:hover': { backgroundColor: 'black' } }}
-        >
-          Home
-        </Button>
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/features"
-          sx={{ '&:hover': { backgroundColor: 'black' } }}
-        >
-          Features
-        </Button>
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/about"
-          sx={{ '&:hover': { backgroundColor: 'black' } }}
-        >
-          About
-        </Button>
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/contact"
-          sx={{ '&:hover': { backgroundColor: 'black' } }}
-        >
-          Contact
-        </Button>
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/login"
-          sx={{ '&:hover': { backgroundColor: 'black' } }}
-        >
-          Log In
-        </Button>
+        {['home', 'features', 'about', 'contact', 'login'].map((path) => (
+          <Button
+            key={path}
+            color="inherit"
+            component={RouterLink}
+            to={`/${path}`}
+            sx={{
+              '&:hover': { backgroundColor: '#782cf6' },
+              backgroundColor: isActive(`/${path}`) ? '#782cf6' : 'transparent',
+            }}
+          >
+            {path.charAt(0).toUpperCase() + path.slice(1)}{' '}
+            {/* Capitalize the first letter */}
+          </Button>
+        ))}
       </Toolbar>
     </AppBar>
   );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 // Import Components
 import HorizontalNav from '../HorizontalNav/HorizontalNav';
 
+// Import Custom CSS
+import './LandingPage.css';
+
+// Import custom image
+import workingoutIMG from './images/person_squatting.jpg';
+
 function LandingPage() {
   return (
     <>
@@ -12,8 +18,20 @@ function LandingPage() {
           <HorizontalNav />
         </div>
         <div className="landing-page-container">
-          <div>
-            <p>Landing Page</p>
+          <div className="clip-triangle">
+            <h2 className="landing-page-header">Be Fit</h2>
+            <h2 className="landing-page-header">Be Healthy</h2>
+            <h2 className="landing-page-header">Be In Control</h2>
+            <p className="landing-page-paragraph">
+              Health Harmony's fitness tracker allows you to take control of
+              <br />
+              your life. We are here to help you understand the correlation
+              <br />
+              between how you feel and your daily fitness
+            </p>
+          </div>
+          <div className="landing-page-image">
+            <img src={workingoutIMG} alt="picture of woman working out" />
           </div>
         </div>
       </div>
