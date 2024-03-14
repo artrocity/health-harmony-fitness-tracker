b29f401ef2c8c0364d9ed7b64dd8aa09f7a398fa
Ryan Barfield <cadium828@gmail.com>
2024-03-13 21:32:13 -0500

    added buttons to landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,12 @@
 // Import 3rd Party Libraries
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
 // Import Components
 import HorizontalNav from '../HorizontalNav/HorizontalNav';
+
+// Material UI CSS
+import Button from '@mui/material/Button';
 
 // Import Custom CSS
 import './LandingPage.css';
@@ -20,12 +24,32 @@ function LandingPage() {
             <h2 className="landing-page-header">Be Healthy</h2>
             <h2 className="landing-page-header">Be In Control</h2>
             <p className="landing-page-paragraph">
-              Health Harmony's fitness tracker allows you to take control of
-              <br />
-              your life. We are here to help you understand the correlation
-              <br />
-              between how you feel and your daily fitness
+              Health Harmony's fitness tracker empowers <br />
+              you to master your wellness journey. Our mission <br />
+              is to guide you in recognizing the vital connection <br /> between
+              your physical state and overall well-being. <br />
+              Through intuitive tracking and insightful analysis, <br />
+              we're dedicated to enhancing your health, one day at a time.
             </p>
+            <div className="button-container">
+              <Button
+                variant="contained"
+                color="primary"
+                component={RouterLink}
+                to="/register"
+              >
+                Sign Up
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                href="https://apps.apple.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download our App
+              </Button>
+            </div>
           </div>
         </div>
       </div>
