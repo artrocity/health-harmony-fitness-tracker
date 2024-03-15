f894eba216850c41181c672ccae40fdfad134c4e
Ryan Barfield <cadium828@gmail.com>
2024-03-15 14:57:16 -0500

    completed view404 component and styling

--- src/components/View404/View404.jsx
+++ src/components/View404/View404.jsx
@@ -4,11 +4,24 @@ import React from 'react';
 // Import Components
 import HorizontalNav from '../HorizontalNav/HorizontalNav';
 
+// Import Image
+import errorImage from './images/404.png';
+
+// Import Custom CSS
+import './View404.css';
+
 function View404() {
   return (
     <div className="container">
-      <div>
-        <p>404 View</p>
+      <HorizontalNav />
+      <div className="view404-page-container">
+        <h1 className="view404-page-header">
+          OOPS! <br /> It looks like you're lost!
+        </h1>
+        <p className="view404-page-paragraph">
+          Either the page you're trying to access doesn't exist <br /> or you
+          don't have authorized access to view it!
+        </p>
       </div>
     </div>
   );
