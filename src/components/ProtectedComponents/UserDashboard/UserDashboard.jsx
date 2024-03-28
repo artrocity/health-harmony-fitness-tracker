58eaa0be15c64ecba1e73f75da09e0348422c9ad
Ryan Barfield <cadium828@gmail.com>
2024-03-27 20:54:24 -0500

    added images and general outline of user dashboard component

--- src/components/ProtectedComponents/UserDashboard/UserDashboard.jsx
+++ src/components/ProtectedComponents/UserDashboard/UserDashboard.jsx
@@ -8,8 +8,11 @@ import VerticalNav from '../VerticalNav/VerticalNav';
 // Import Material UI
 import { Grid, Typography } from '@mui/material';
 
-// Import Custom CSS
+// Import Custom CSS and Images
 import './UserDashboard.css';
+import plantImage from './images/plant_based.avif';
+import paleoImage from './images/paleo.avif';
+import lowCarbImage from './images/low_carb.avif';
 
 function UserDashboard() {
   const user = useSelector((store) => store.user);
@@ -22,7 +25,73 @@ function UserDashboard() {
         </div>
         <div className="page-right-container">
           {user && user.id ? <h1>Welcome Back, {user.name}!</h1> : ''}
-          <Grid></Grid>
+          <Grid
+            container
+            spacing={1}
+            wrap="wrap"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} md={6}>
+              <h2>Calories Div</h2>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <h2>Weight Div</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <h2>Activity Div</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography
+                variant="h5"
+                style={{ textAlign: 'left', marginBottom: '0px' }}
+              >
+                Diet Plans
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <a
+                href="https://www.health.harvard.edu/blog/what-is-a-plant-based-diet-and-why-should-you-try-it-2018092614760"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="diet-plan-container"
+                  style={{ backgroundImage: `url(${plantImage})` }}
+                >
+                  <h2 className="diet-header">Plant Based</h2>
+                </div>
+              </a>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <a
+                href="https://www.healthline.com/nutrition/paleo-diet-meal-plan-and-menu"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="diet-plan-container"
+                  style={{ backgroundImage: `url(${paleoImage})` }}
+                >
+                  <h2 className="diet-header">Paleo</h2>
+                </div>
+              </a>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <a
+                href="https://www.healthline.com/nutrition/low-carb-diet-meal-plan-and-menu"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div
+                  className="diet-plan-container"
+                  style={{ backgroundImage: `url(${lowCarbImage})` }}
+                >
+                  <h2 className="diet-header">Low-Carb</h2>
+                </div>
+              </a>
+            </Grid>
+          </Grid>
         </div>
       </div>
     </>
