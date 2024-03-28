764fc6a842531c34627511b5f9b75a8eda496636
Ryan Barfield <cadium828@gmail.com>
2024-03-27 21:14:01 -0500

    initial styling for how dashboard components will be placed is completed however, they are too big and height or layout needs to be adjusted

--- src/components/ProtectedComponents/UserDashboard/UserDashboard.jsx
+++ src/components/ProtectedComponents/UserDashboard/UserDashboard.jsx
@@ -1,5 +1,5 @@
 // Import 3rd Party Libraries
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 // Import Custom Components
@@ -24,74 +24,85 @@ function UserDashboard() {
           <VerticalNav />
         </div>
         <div className="page-right-container">
-          {user && user.id ? <h1>Welcome Back, {user.name}!</h1> : ''}
-          <Grid
-            container
-            spacing={1}
-            wrap="wrap"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} md={6}>
-              <h2>Calories Div</h2>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <h2>Weight Div</h2>
-            </Grid>
-            <Grid item xs={12}>
-              <h2>Activity Div</h2>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography
-                variant="h5"
-                style={{ textAlign: 'left', marginBottom: '0px' }}
-              >
-                Diet Plans
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <a
-                href="https://www.health.harvard.edu/blog/what-is-a-plant-based-diet-and-why-should-you-try-it-2018092614760"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div
-                  className="diet-plan-container"
-                  style={{ backgroundImage: `url(${plantImage})` }}
-                >
-                  <h2 className="diet-header">Plant Based</h2>
+          <div className="dashboard-container">
+            {user && user.id ? <h1>Welcome Back, {user.name}!</h1> : ''}
+            <Grid
+              container
+              spacing={1}
+              wrap="wrap"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} md={6}>
+                <div className="dashboard-calories-container">
+                  <h2>Calories Div</h2>
                 </div>
-              </a>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <a
-                href="https://www.healthline.com/nutrition/paleo-diet-meal-plan-and-menu"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div
-                  className="diet-plan-container"
-                  style={{ backgroundImage: `url(${paleoImage})` }}
-                >
-                  <h2 className="diet-header">Paleo</h2>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <div className="dashboard-weight-container">
+                  <h2>Weight Div</h2>
                 </div>
-              </a>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <a
-                href="https://www.healthline.com/nutrition/low-carb-diet-meal-plan-and-menu"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div
-                  className="diet-plan-container"
-                  style={{ backgroundImage: `url(${lowCarbImage})` }}
-                >
-                  <h2 className="diet-header">Low-Carb</h2>
+              </Grid>
+              <Grid item xs={12}>
+                <div className="dashboard-activity-container">
+                  <h2>Activity Div</h2>
                 </div>
-              </a>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography
+                  variant="h5"
+                  style={{ textAlign: 'left', marginBottom: '0px' }}
+                >
+                  Diet Plans
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <a
+                  href="https://www.health.harvard.edu/blog/what-is-a-plant-based-diet-and-why-should-you-try-it-2018092614760"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="diet-external-link"
+                >
+                  <div
+                    className="diet-plan-container"
+                    style={{ backgroundImage: `url(${plantImage})` }}
+                  >
+                    <h2 className="diet-header">Plant Based</h2>
+                  </div>
+                </a>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <a
+                  href="https://www.healthline.com/nutrition/paleo-diet-meal-plan-and-menu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="diet-external-link"
+                >
+                  <div
+                    className="diet-plan-container"
+                    style={{ backgroundImage: `url(${paleoImage})` }}
+                  >
+                    <h2 className="diet-header">Paleo</h2>
+                  </div>
+                </a>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <a
+                  href="https://www.healthline.com/nutrition/low-carb-diet-meal-plan-and-menu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="diet-external-link"
+                >
+                  <div
+                    className="diet-plan-container"
+                    style={{ backgroundImage: `url(${lowCarbImage})` }}
+                  >
+                    <h2 className="diet-header">Low-Carb</h2>
+                  </div>
+                </a>
+              </Grid>
             </Grid>
-          </Grid>
+          </div>
         </div>
       </div>
     </>
