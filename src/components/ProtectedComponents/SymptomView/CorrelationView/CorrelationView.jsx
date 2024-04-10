1bb9820024f9cf2a5cd0584fa00badbfcb777136
Ryan Barfield <cadium828@gmail.com>
2024-04-10 10:38:21 -0500

    initial implementation of correlation view completed

---
+++ src/components/ProtectedComponents/SymptomView/CorrelationView/CorrelationView.jsx
@@ -0,0 +1,34 @@
+// Import 3rd Party Libraries
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// Import Custom Components
+import VerticalNav from '../VerticalNav/VerticalNav';
+
+// Import MaterialUI
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
+
+function CorrelationView() {
+  return (
+    <>
+      <div className="page-container">
+        <div className="nav-container">
+          <VerticalNav />
+        </div>
+        <div className="page-right-container">
+          <p>Here are the possible reasons for your symptoms</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CorrelationView;
